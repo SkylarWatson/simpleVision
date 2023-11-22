ee684b32095e44bca9c6cbbdbd1457dfbc773290
Skylar <skylarrwatson@gmail.com>
2023-11-21 22:45:38 -0600

    moved lens service into the lens package to decouple lens from patient information

---
+++ simple-vision-core/src/main/java/com/simplevision/lens/service/LensService.java
@@ -0,0 +1,22 @@
+package com.simplevision.lens.service;
+
+import com.simplevision.core.domain.Lens;
+import com.simplevision.core.mapper.LensMapper;
+import com.simplevision.core.repository.LensRepository;
+import com.simplevision.core.view.LensView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LensService {
+    @Autowired private LensMapper lensMapper;
+    @Autowired private LensRepository lensRepository;
+
+    public Lens create(LensView lens) {
+        return lensRepository.save(lensMapper.map(lens));
+    }
+
+    public LensView findLensById(long id) {
+        return lensMapper.map(lensRepository.findById(id).get());
+    }
+}
