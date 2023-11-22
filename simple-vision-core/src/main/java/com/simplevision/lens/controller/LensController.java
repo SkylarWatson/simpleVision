c7f936887285456443f3d6c4653b720fea5e8970
Skylar <skylarrwatson@gmail.com>
2023-11-21 22:34:21 -0600

    moved lens controller into the lens package to decouple lens from patient information

---
+++ simple-vision-core/src/main/java/com/simplevision/lens/controller/LensController.java
@@ -0,0 +1,26 @@
+package com.simplevision.lens.controller;
+
+import com.simplevision.core.service.InfoService;
+import com.simplevision.core.view.LensView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class LensController {
+    @Autowired private InfoService service;
+
+    @PostMapping("/lens")
+    public void create(@RequestBody LensView lens) {
+        service.create(lens);
+    }
+
+    @GetMapping("/lens")
+    public LensView findLensById(int id) {
+        return service.findLensById(id);
+    }
+}
