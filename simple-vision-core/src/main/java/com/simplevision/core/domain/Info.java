25dc821acac5a9710c34c96a75dde7ed7b25a18e
Skylar <skylarrwatson@gmail.com>
2023-11-21 19:33:45 -0600

    removed unused fields from Info

--- simple-vision-core/src/main/java/com/simplevision/core/domain/Info.java
+++ simple-vision-core/src/main/java/com/simplevision/core/domain/Info.java
@@ -13,16 +13,6 @@ public class Info {
 
     private String name;
     private String address;
-    private String leftSphere;
-    private String leftCylinder;
-    private String leftAxis;
-    private String rightSphere;
-    private String rightCylinder;
-    private String rightAxis;
-    private String PD;
-    private String lensMaterial;
-    private String lensAdditions;
-    private String lensStyle;
     private String email;
     private String phoneNumber;
 
@@ -40,70 +30,6 @@ public class Info {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public String getLeftSphere() {
-        return leftSphere;
-    }
-
-    public void setLeftSphere(String leftSphere) {
-        this.leftSphere = leftSphere;
-    }
-
-    public String getLeftCylinder() {
-        return leftCylinder;
-    }
-
-    public void setLeftCylinder(String leftCylinder) {
-        this.leftCylinder = leftCylinder;
-    }
-
-    public String getLeftAxis() {
-        return leftAxis;
-    }
-
-    public void setLeftAxis(String leftAxis) {
-        this.leftAxis = leftAxis;
-    }
-
-    public String getRightSphere() {
-        return rightSphere;
-    }
-
-    public void setRightSphere(String rightSphere) {
-        this.rightSphere = rightSphere;
-    }
-
-    public String getRightCylinder() {
-        return rightCylinder;
-    }
-
-    public void setRightCylinder(String rightCylinder) {
-        this.rightCylinder = rightCylinder;
-    }
-
-    public String getRightAxis() {
-        return rightAxis;
-    }
-
-    public void setRightAxis(String rightAxis) {
-        this.rightAxis = rightAxis;
-    }
-
-    public String getPD() {
-        return PD;
-    }
-
-    public void setPD(String PD) {
-        this.PD = PD;
-    }
-
-    public String getLensMaterial() {
-        return lensMaterial;
-    }
-
-    public void setLensMaterial(String lensMaterial) {
-        this.lensMaterial = lensMaterial;
     }
 
     public String getEmail() {
@@ -128,22 +54,6 @@ public class Info {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getLensAdditions() {
-        return lensAdditions;
-    }
-
-    public void setLensAdditions(String lensCoating) {
-        this.lensAdditions = lensCoating;
-    }
-
-    public String getLensStyle() {
-        return lensStyle;
-    }
-
-    public void setLensStyle(String visionType) {
-        this.lensStyle = visionType;
     }
 }
 
