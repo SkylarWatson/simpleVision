77705a51f70e178a9ab362d36be1f17cb6b4bc71
Skylar <skylarrwatson@gmail.com>
2023-11-24 15:56:24 -0600

    copied the prescription code into the new service

---
+++ simple-vision-prescriptions/src/main/java/com/simplevision/prescription/view/PrescriptionDetails.java
@@ -0,0 +1,31 @@
+package com.simplevision.prescription.view;
+
+public class PrescriptionDetails {
+    String sphere;
+    String cylinder;
+    String axis;
+
+    public String getSphere() {
+        return sphere;
+    }
+
+    public void setSphere(String sphere) {
+        this.sphere = sphere;
+    }
+
+    public String getCylinder() {
+        return cylinder;
+    }
+
+    public void setCylinder(String cylinder) {
+        this.cylinder = cylinder;
+    }
+
+    public String getAxis() {
+        return axis;
+    }
+
+    public void setAxis(String axis) {
+        this.axis = axis;
+    }
+}
---
+++ simple-vision-prescriptions/src/test/java/com/simplevision/prescription/controller/PrescriptionControllerTest.java
@@ -0,0 +1,38 @@
+package com.simplevision.prescription.controller;
+
+import com.simplevision.prescription.view.PrescriptionView;
+import com.simplevision.prescription.service.PrescriptionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PrescriptionControllerTest {
+    @InjectMocks private PrescriptionController controller;
+    @Mock private PrescriptionService service;
+
+    @Test
+    public void createPrescription() {
+        PrescriptionView prescription = new PrescriptionView();
+
+        controller.create(prescription);
+
+        verify(service).create(prescription);
+    }
+
+    @Test
+    public void findPrescriptionById() {
+        PrescriptionView prescription = new PrescriptionView();
+
+        when(service.findPrescriptionById(anyLong())).thenReturn(prescription);
+
+        assertEquals(prescription, controller.findPrescriptionById(1));
+    }
+}
