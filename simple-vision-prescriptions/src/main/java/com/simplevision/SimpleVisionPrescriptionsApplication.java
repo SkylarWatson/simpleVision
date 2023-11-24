802ac6e66d45d1422f2edb3d75299a586d34e9c6
Skylar <skylarrwatson@gmail.com>
2023-11-24 15:51:56 -0600

    initialized new prescription service

---
+++ simple-vision-prescriptions/src/main/java/com/simplevision/SimpleVisionPrescriptionsApplication.java
@@ -0,0 +1,13 @@
+package com.simplevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVisionPrescriptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVisionPrescriptionsApplication.class, args);
+	}
+
+}
