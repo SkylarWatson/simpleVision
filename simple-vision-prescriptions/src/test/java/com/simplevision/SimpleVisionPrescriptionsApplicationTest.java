19eccfe254cc718ec5f664d6c5db76d42f32ee54
Skylar <skylarrwatson@gmail.com>
2023-11-24 15:51:56 -0600

    initialized new prescription service

---
+++ simple-vision-prescriptions/src/test/java/com/simplevision/SimpleVisionPrescriptionsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.simplevision;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleVisionPrescriptionsApplicationTest {
+    @Test
+    public void applicationTest() {
+        assertTrue(true);
+    }
+}
